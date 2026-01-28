f6ac273bbf7a4083eac99b8a57a500ea565020d3
Karol Kozlowski <karol.kozlowski@dotnot.pl>
2026-01-28 19:21:17 +0100

    Add more configuration options

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,8 @@ LABEL org.label-schema.build-date="${BUILD_DATE}" \
 COPY src/debconf-selections /var/lib/debconf-selections
 RUN debconf-set-selections /var/lib/debconf-selections
 
+ENV DEBIAN_FRONTEND=noninteractive
+
 RUN apt-get -y update &&  \
     apt-get install --no-install-recommends -y vsftpd libpam-ldapd libnss-ldap ssl-cert && \
     rm -rf /var/lib/apt/lists/* && \
@@ -35,15 +37,46 @@ ENV LDAP_URI=ldaps://ldap.example.com \
     LOCAL_UMASK=022 \
     ANON_UPLOAD_ENABLE=NO \
     ANON_MKDIR_WRITE_ENABLE=NO \
-    DIRMESSAGE_ENABLE=yes \
+    DIRMESSAGE_ENABLE=YES \
     USE_LOCALTIME=YES \
     XFERLOG_ENABLE=YES \
+    XFERLOG_STD_FORMAT=NO \
+    LOG_FTP_PROTOCOL=NO \
+    SYSLOG_ENABLE=NO \
+    VSFTPD_LOG_FILE=/var/log/vsftpd.log \
     IDLE_SESSION_TIMEOUT=600 \
+    DATA_CONNECTION_TIMEOUT=300 \
+    ACCEPT_TIMEOUT=60 \
+    CONNECT_TIMEOUT=60 \
     RATE_MAX_PER_IP=5 \
+    MAX_CLIENTS=0 \
+    MAX_LOGIN_FAILS=3 \
+    DELAY_FAILED_LOGIN=1 \
+    PORT_ENABLE=YES \
+    CONNECT_FROM_PORT_20=NO \
+    FTP_DATA_PORT=20 \
+    FILE_OPEN_MODE=0666 \
     SSL_ENABLE=YES \
+    SSL_TLSV1=YES \
+    SSL_SSLV2=NO \
+    SSL_SSLV3=NO \
     SSL_CIPHERS=HIGH \
+    FORCE_LOCAL_DATA_SSL=YES \
+    FORCE_LOCAL_LOGINS_SSL=YES \
+    REQUIRE_SSL_REUSE=NO \
     RSA_CERT_FILE= \
     RSA_PRIVATE_KEY_FILE= \
+    SESSION_SUPPORT=YES \
+    HIDE_IDS=NO \
+    LS_RECURSE_ENABLE=NO \
+    DOWNLOAD_ENABLE=YES \
+    DIRLIST_ENABLE=YES \
+    CHMOD_ENABLE=YES \
+    DELETE_FAILED_UPLOADS=NO \
+    ASCII_UPLOAD_ENABLE=NO \
+    ASCII_DOWNLOAD_ENABLE=NO \
+    SETPROCTITLE_ENABLE=NO \
+    TEXT_USERDB_NAMES=NO \
     FTPD_BANNER="Welcome to FTP service." \
     PASV_ENABLE=YES \
     PASV_ADDRESS= \
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,9 +5,11 @@ set -e
 set -o pipefail
 
 # set timezone
-if [ ! -f /etc/timezone ] && [ -n "$TZ" ]; then
-  cp "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
-  echo "${TZ}" > /etc/timezone
+if [ -n "$TZ" ]; then
+  if [ ! -f /etc/timezone ] || [ "$(cat /etc/timezone)" != "$TZ" ]; then
+    ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
+    echo "${TZ}" > /etc/timezone
+  fi
 fi
 
 if [ -z "${RSA_CERT_FILE}" ] || [ -z "${RSA_PRIVATE_KEY_FILE}" ]; then
@@ -66,133 +68,97 @@ fi
 
 # vsftpd
 cat > /etc/vsftpd.conf <<EOF
+# Standalone mode
 listen=YES
+background=NO
 
-# Configure passive mode
+# Access controls
+anonymous_enable=${ANONYMOUS_ENABLE}
+local_enable=${LOCAL_ENABLE}
+write_enable=${WRITE_ENABLE}
+anon_upload_enable=${ANON_UPLOAD_ENABLE}
+anon_mkdir_write_enable=${ANON_MKDIR_WRITE_ENABLE}
+
+# Security and chroot
+chroot_local_user=YES
+allow_writeable_chroot=YES
+secure_chroot_dir=/var/run/vsftpd/empty
+
+# Passive mode configuration
 pasv_enable=${PASV_ENABLE}
 pasv_address=${PASV_ADDRESS}
 pasv_min_port=${PASV_MIN_PORT}
 pasv_max_port=${PASV_MAX_PORT}
 
+# Active mode configuration
+port_enable=${PORT_ENABLE}
+connect_from_port_20=${CONNECT_FROM_PORT_20}
+ftp_data_port=${FTP_DATA_PORT}
 
-# Allow anonymous FTP? (Disabled by default).
-anonymous_enable=${ANONYMOUS_ENABLE}
-
-# Uncomment this to allow local users to log in.
-local_enable=${LOCAL_ENABLE}
-
-# Uncomment this to enable any form of FTP write command.
-write_enable=${WRITE_ENABLE}
-
-allow_writeable_chroot=YES
-
-# Default umask for local users is 077. You may wish to change this to 022,
-# if your users expect that (022 is used by most other ftpd's)
+# File permissions
 local_umask=${LOCAL_UMASK}
+file_open_mode=${FILE_OPEN_MODE}
 
-# Uncomment this to allow the anonymous FTP user to upload files. This only
-# has an effect if the above global write enable is activated. Also, you will
-# obviously need to create a directory writable by the FTP user.
-anon_upload_enable=${ANON_UPLOAD_ENABLE}
-
-# Uncomment this if you want the anonymous FTP user to be able to create
-# new directories.
-anon_mkdir_write_enable=${ANON_MKDIR_WRITE_ENABLE}
-
-# Activate directory messages - messages given to remote users when they
-# go into a certain directory.
-dirmessage_enable=${DIRMESSAGE_ENABLE}
-
-# If enabled, vsftpd will display directory listings with the time
-# in  your  local  time  zone.  The default is to display GMT. The
-# times returned by the MDTM FTP command are also affected by this
-# option.
-use_localtime=${USE_LOCALTIME}
-
-# Activate logging of uploads/downloads.
-xferlog_enable=${XFERLOG_ENABLE}
-
-# Make sure PORT transfer connections originate from port 20 (ftp-data).
-connect_from_port_20=YES
-
-# If you want, you can arrange for uploaded anonymous files to be owned by
-# a different user. Note! Using "root" for uploaded files is not
-# recommended!
-#chown_uploads=YES
-#chown_username=whoever
-
-# You may change the default value for timing out an idle session.
+# Timeouts
 idle_session_timeout=${IDLE_SESSION_TIMEOUT}
+data_connection_timeout=${DATA_CONNECTION_TIMEOUT}
+accept_timeout=${ACCEPT_TIMEOUT}
+connect_timeout=${CONNECT_TIMEOUT}
 
-# You may change the default value for timing out a data connection.
-#data_connection_timeout=120
+# Rate limiting
+max_per_ip=${RATE_MAX_PER_IP}
+max_clients=${MAX_CLIENTS}
+max_login_fails=${MAX_LOGIN_FAILS}
+delay_failed_login=${DELAY_FAILED_LOGIN}
 
-# It is recommended that you define on your system a unique user which the
-# ftp server can use as a totally isolated and unprivileged user.
-#nopriv_user=ftpsecure
+# Logging
+xferlog_enable=${XFERLOG_ENABLE}
+xferlog_std_format=${XFERLOG_STD_FORMAT}
+log_ftp_protocol=${LOG_FTP_PROTOCOL}
+syslog_enable=${SYSLOG_ENABLE}
+vsftpd_log_file=${VSFTPD_LOG_FILE}
 
-# Enable this and the server will recognise asynchronous ABOR requests. Not
-# recommended for security (the code is non-trivial). Not enabling it,
-# however, may confuse older FTP clients.
-#async_abor_enable=YES
-
-# By default the server will pretend to allow ASCII mode but in fact ignore
-# the request. Turn on the below options to have the server actually do ASCII
-# mangling on files when in ASCII mode.
-# Beware that on some FTP servers, ASCII support allows a denial of service
-# attack (DoS) via the command "SIZE /big/file" in ASCII mode. vsftpd
-# predicted this attack and has always been safe, reporting the size of the
-# raw file.
-# ASCII mangling is a horrible feature of the protocol.
-#ascii_upload_enable=YES
-#ascii_download_enable=YES
-
-# You may fully customise the login banner string:
+# Messages and banners
+dirmessage_enable=${DIRMESSAGE_ENABLE}
 ftpd_banner=${FTPD_BANNER}
 
-# You may specify a file of disallowed anonymous e-mail addresses. Apparently
-# useful for combatting certain DoS attacks.
-deny_email_enable=YES
-# (default follows)
-banned_email_file=/etc/vsftpd.banned_emails
+# Locale
+use_localtime=${USE_LOCALTIME}
+utf8_filesystem=YES
 
-# You may restrict local users to their home directories.  See the FAQ for
-# the possible risks in this before using chroot_local_user or
-# chroot_list_enable below.
-chroot_local_user=YES
-
-# You may activate the "-R" option to the builtin ls. This is disabled by
-# default to avoid remote users being able to cause excessive I/O on large
-# sites. However, some broken FTP clients such as "ncftp" and "mirror" assume
-# the presence of the "-R" option, so there is a strong case for enabling it.
-#ls_recurse_enable=YES
-
-# Customization
-#
-# Some of vsftpd's settings don't fit the filesystem layout by
-# default.
-
-# This option should be the name of a directory which is empty.  Also, the
-# directory should not be writable by the ftp user. This directory is used
-# as a secure chroot() jail at times vsftpd does not require filesystem
-# access.
-secure_chroot_dir=/var/run/vsftpd/empty
-
-# This string is the name of the PAM service vsftpd will use.
-pam_service_name=vsftpd
-
-# This option specifies the location of the RSA certificate to use for SSL
-# encrypted connections.
+# SSL/TLS configuration
 ssl_enable=${SSL_ENABLE}
+ssl_tlsv1=${SSL_TLSV1}
+ssl_sslv2=${SSL_SSLV2}
+ssl_sslv3=${SSL_SSLV3}
 ssl_ciphers=${SSL_CIPHERS}
 rsa_cert_file=${RSA_CERT_FILE}
 rsa_private_key_file=${RSA_PRIVATE_KEY_FILE}
+force_local_data_ssl=${FORCE_LOCAL_DATA_SSL}
+force_local_logins_ssl=${FORCE_LOCAL_LOGINS_SSL}
+require_ssl_reuse=${REQUIRE_SSL_REUSE}
 
-# rate limiting
-max_per_ip=${RATE_MAX_PER_IP}
+# PAM integration
+pam_service_name=vsftpd
+session_support=${SESSION_SUPPORT}
 
-# Uncomment this to indicate that vsftpd use a utf8 filesystem.
-utf8_filesystem=YES
+# Additional security
+hide_ids=${HIDE_IDS}
+ls_recurse_enable=${LS_RECURSE_ENABLE}
+download_enable=${DOWNLOAD_ENABLE}
+dirlist_enable=${DIRLIST_ENABLE}
+chmod_enable=${CHMOD_ENABLE}
+delete_failed_uploads=${DELETE_FAILED_UPLOADS}
+ascii_upload_enable=${ASCII_UPLOAD_ENABLE}
+ascii_download_enable=${ASCII_DOWNLOAD_ENABLE}
+
+# Email restrictions for anonymous (if enabled)
+deny_email_enable=YES
+banned_email_file=/etc/vsftpd.banned_emails
+
+# Process management
+setproctitle_enable=${SETPROCTITLE_ENABLE}
+text_userdb_names=${TEXT_USERDB_NAMES}
 EOF
 
 # ensure banned email list is present
